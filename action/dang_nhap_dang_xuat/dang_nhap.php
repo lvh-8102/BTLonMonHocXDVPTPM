c22e933938a7f0ced13a71f62ffbc5e5e230a04f
godOfWibu000 <hoangluonga4uhb@gmail.com>
2024-01-14 08:23:42 +0700

    update 8:23 14/01/2024

--- action/dang_nhap_dang_xuat/dang_nhap.php
+++ action/dang_nhap_dang_xuat/dang_nhap.php
@@ -8,14 +8,16 @@
         if($taiKhoan->num_rows<1){
             echo 'Tài khoản không tồn tại, vui lòng kiểm tra lại!';
         }else{
-            while($row = $taiKhoan->fetch_assoc())
+            while($row = $taiKhoan->fetch_assoc()){
                 $matKhau = $row['MatKhau'];
+                $loaiTaiKhoan = $row['LoaiTaiKhoan'];
+            }
             if($matKhau != $_POST['matKhau'])
                 echo 'Sai mật khẩu, vui lòng kiểm tra lại!';
             else{
                 echo('Đăng nhập thành công!');
                 $_SESSION['ten-dang-nhap'] = $_POST['tenDangNhap'];
-                $_SESSION['loai-tai-khoan'] = $_POST['LoaiTaiKhoan'];
+                $_SESSION['loai-tai-khoan'] = $loaiTaiKhoan;
             }
 
         }
